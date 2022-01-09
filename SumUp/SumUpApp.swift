896bb251d83de939665ee8454c5005e610b1ac22
Max <maximilian@dahos.de>
2022-01-09 10:53:00 +0100

    Initial Commit

---
+++ SumUp/SumUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SumUpApp.swift
+//  SumUp
+//
+//  Created by Maximilian Schröder on 09.01.22.
+//
+
+import SwiftUI
+
+@main
+struct SumUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
